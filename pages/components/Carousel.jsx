d52e2ecbefe629a6277abcd2d67231dbbbeedcfc
aniquehussain <47793113+aniquehussain@users.noreply.github.com>
2020-10-16 16:27:04 +0530

    Completed Test commit -4.5

--- pages/components/Carousel.jsx
+++ pages/components/Carousel.jsx
@@ -2,7 +2,7 @@ const Carousel = () => {
     return (
         <div className="carousel-box">
             <p className="text-l text-orange-500">Code to the Future</p>
-            <h1 className="font-bold text-white text-4xl ">Welcome to Turing Ninjas!</h1>
+            <h1 className="font-bold text-white text-4xl sm:text-lg">Welcome to Turing Ninjas!</h1>
             <button className="bg-yellow-500 text-white hover:bg-red-700  rounded-full">LEARN MORE</button>
             <div id="carouselExampleSlidesOnly" className="carousel slide w-screen" data-ride="carousel" >
                 <div className="carousel-inner" >
